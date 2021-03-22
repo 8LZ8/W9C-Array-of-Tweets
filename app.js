7fdbae653ea0fabeeee4c2182ed8dd433fa2ef53
L <8lzkg8@gmail.com>
2021-03-22 17:22:54 -0600

    W9E: Advanced Array Operations and Functions

--- app.js
+++ app.js
@@ -1,19 +1,78 @@
-// Making an Array with 10 of my favorite Socrates quotes
+// Referred back to in-class exercise and used the examples given for this assignment.
+// Making an Array of 10 Javacript Objects. 
 var tweets = [
-    `One thing only I know, and that is that I know nothing.`,
-    `Happiness is unrepentant pleasure.`,
-    `Be as you wish to seem.`,
-    `Know thyself.`,
-    `The highest realms of thought are impossible to reach without first attaining an understanding of compassion.`,
-    `The nearest way to glory is to strive to be what you wish to be thought to be.`,
-    `The secret of change is to focus all your energy, not in fighting the old, but on building the new.`,
-    `Falling down is not a failure. Failure comes when you stay where you have fallen.`,
-    `The greatest blessing granted to mankind come by way of madness which is a divine gift.`,
-    `Wisdom begins in wonder.`,
+    {
+        tweet: `One thing only I know, and that is that I know nothing.`,
+        username: `Socrates`, 
+        created_at: `03-20-2021`,
+        age: 15
+    },
+    {
+        tweet: `Happiness is unrepentant pleasure.`,
+        username: `Socrates`, 
+        created_at: `03-20-2021`,
+        age: 15
+    },
+    {
+        tweet: `Falling down is not a failure. Failure comes when you stay where you have fallen.`,
+        username: `Socrates`, 
+        created_at: `03-20-2021`,
+        age: 15
+    },
+    {
+        tweet: `Peace can only come as a natural consequence of universal enlightenment.`,
+        username: `Nikola Tesla`, 
+        created_at: `03-22-2021`,
+        age: 86
+    },
+    {
+        tweet: `The highest realms of thought are impossible to reach without first attaining an understanding of compassion.`,
+        username: `Socrates`, 
+        created_at: `03-20-2021`,
+        age: 15
+    },
+    {
+        tweet: `The secret of change is to focus all your energy, not in fighting the old, but on building the new.`,
+        username: `Socrates`, 
+        created_at: `03-20-2021`,
+        age: 15
+    },
+    {
+        tweet: `The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane.`,
+        username: `Nikola Tesla`, 
+        created_at: `03-22-2021`,
+        age: 86
+    },
+    {
+        tweet: `Wisdom begins in wonder.`,
+        username: `Socrates`, 
+        created_at: `03-20-2021`,
+        age: 15
+    },
+    {
+        tweet: `Of all things, I liked books best.`,
+        username: `Nikola Tesla`, 
+        created_at: `03-22-2021`,
+        age: 86
+    },
+    {
+        tweet: `My brain is only a receiver, in the Universe there is a core from which we obtain knowledge, strength and inspiration. I have not penetrated into the secrets of this core, but I know that it exists.`,
+        username: `Nikola Tesla`, 
+        created_at: `03-22-2021`,
+        age: 86
+    },  
 ];
 
-// Looping 10 times
-for (var counter = 0; counter < 10; counter++) {
-    //Printing the last tweet and pop (removes it)
-    console.log(tweets.pop());
+// Made a function;checks for age 18 and older returns true, if otherwise false
+function checkAge(tweetPost) {
+    if (tweetPost.age >= 18) {
+        return true;
+    } else if (tweetPost.age < 18) {
+        return false;
+    }  
 }
+// Filter the age for tweets that are made by users 18 and over
+var checkOver18 = tweets.filter(checkAge);
+console.log(checkOver18);
+
+
